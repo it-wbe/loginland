1c1dc7153a9097b872f2a3afbd9b1e1f395054a5
00rane00 <00rane00@gmail.com>
2017-10-13 10:35:17 +0300

    fix style

--- src/views/auth/authModal.blade.php
+++ src/views/auth/authModal.blade.php
@@ -1,9 +1,9 @@
-
+@section('styles')
 <link rel="stylesheet" href="/packages/wbe/loginland/assets/css/auth.css">
 {{--<link rel="stylesheet" href="/packages/wbe/loginland/assets/css/app.css">--}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
+@endsection
 <div id="head_auth" class="modal fade" role="dialog">
     <div class="modal-dialog" style="width: 1000px;">
 	{{--{!!dump(Auth::user())!!}--}}
--- src/views/auth/recoverpass.blade.php
+++ src/views/auth/recoverpass.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
+@section('styles')
+<link rel="stylesheet" href="/packages/wbe/loginland/assets/css/auth.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+@endsection
 @section('content')
         <div class="modal-dialog" style="max-width: 800px; z-index:99999;"">
             <!-- Modal content-->
